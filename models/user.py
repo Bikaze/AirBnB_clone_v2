71e1135638976327e0cb9c6e074d08242d9f72a1
ndiramiye2 <onlythenotes@gmail.com>
2024-03-21 14:26:51 +0200

    Nitafanya hivyo peke yangu

--- models/user.py
+++ models/user.py
@@ -15,4 +15,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-    reviews = 
+    reviews = ""
