3bd134b90ac8a858c67be5ac419281b210433df6
bikaze <clmntmugisha@gmail.com>
2024-03-21 20:50:15 +0200

    Updating some models files

--- models/review.py
+++ models/review.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
-from models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 
-
-class Review(BaseModel, Base):
+class Review(BaseModel):
     """ Review classto store review information """
-    __tablename__ = "reviews"
-
-    place_id = Column(String(60), nullable=False, Foreignkey(places.id))
-    user_id = Column(String(60), nullable=False, Foreignkey(users.id))
-    text = Column(String(1024), nullable=False)
+    place_id = ""
+    user_id = ""
+    text = ""
