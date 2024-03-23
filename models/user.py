b572d4e5a50a7f9b2b769f5c080fb20ac1e441cc
bikaze <clmntmugisha@gmail.com>
2024-03-23 20:18:21 +0200

    Updated models/user.py

--- models/user.py
+++ models/user.py
@@ -14,8 +14,8 @@ class User(BaseModel, Base):
         __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
     else:
         email = ""
         password = ""
