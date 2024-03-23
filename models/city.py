78529e4ceda4397593c987c20773987bf2ca0818
bikaze <clmntmugisha@gmail.com>
2024-03-23 20:58:11 +0200

    Upgraded models/places.py

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -11,6 +12,7 @@ class City(BaseModel, Base):
         __tablename__ = "cities"
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
         name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities", cascade="all, delete")
     else:
         name = ""
         state_id = ""
--- models/user.py
+++ models/user.py
@@ -16,6 +16,7 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128))
         last_name = Column(String(128))
+        places = relationship("Place", backref="user", cascade="all, delete")
     else:
         email = ""
         password = ""
