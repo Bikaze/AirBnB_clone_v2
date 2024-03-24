aa6a89413dce58410713f285acc3404a1e42a10a
bikaze <clmntmugisha@gmail.com>
2024-03-24 02:49:48 +0200

    ck

--- models/base_model.py
+++ models/base_model.py
@@ -22,10 +22,11 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if kwargs:
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     setattr(self, key, datetime.fromisoformat(value))
