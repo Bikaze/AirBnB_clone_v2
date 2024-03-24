7924d605902b89f1b53ddfdd3829b61e2ff66159
bikaze <clmntmugisha@gmail.com>
2024-03-24 12:44:32 +0200

    Updated console.py

--- console.py
+++ console.py
@@ -148,7 +148,7 @@ class HBNBCommand(cmd.Cmd):
                     dct[obj[0]] = val
 # arg = f'{clss} {new_instance.id} {dct}'
 # if models.storage_t == 'db':
-                    new_instance.__dict__.update(dct)
+            new_instance.__dict__.update(dct)
         new_instance.save()
 # self.do_update(arg)
 #     else:
