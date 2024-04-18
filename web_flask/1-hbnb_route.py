421dac4cd405b609633f37b4340069e19372c826
bikaze <clmntmugisha@gmail.com>
2024-04-18 22:50:26 +0200

    Create 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This is a script that starts a Flask web application: listening on
-0.0.0.0, port 5000, and Routes: /: display “Hello HBNB!”"""
+0.0.0.0, port 5000, and implements Routes: /, and /hbnb"""
 
 from flask import Flask
 
@@ -11,6 +11,12 @@ app = Flask(__name__)
 def hello_hbnb():
     """This function implements for the route '/'"""
     return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def just_hbnb():
+    """This function implements for the route '/hbnb'"""
+    return "HBNB"
 
 
 app.run(host="0.0.0.0", port=5000)
