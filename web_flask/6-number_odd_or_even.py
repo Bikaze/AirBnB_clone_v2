2a6bdec44b73086d261460b3dade62577a97e3b3
bikaze <clmntmugisha@gmail.com>
2024-04-19 17:35:39 +0200

    Create 6-number_odd_or_even.html

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""This is a script that starts a Flask web application: listening on
+0.0.0.0, port 5000, and implements Routes: /, /hbnb, /c/<text>, /python/,
+/python/<text>, /number/<int:n>, /number_template/<int:n>, and
+ /number_odd_or_even/<int:n>"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """This function implements for the route '/'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def just_hbnb():
+    """This function implements for the route '/hbnb'"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def variable_rule_c(text):
+    """This function implements for the route '/c/<text>'"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def variable_rule_py(text="is cool"):
+    """This function implements for the route '/python/<text>'"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def variable_rule_nbr(n):
+    """This function implements for the route '/number/<int:n>'"""
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def variable_rule_nbr_template(n):
+    """This function implements for the route '/number_template/<int:n>'"""
+    return render_template('5-number.html', number=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def variable_rule_nbr_odd_or_even(n):
+    """This function implements for the route '/number_odd_or_even/<int:n>'"""
+    p = 'even' if n % 2 == 0 else 'odd'
+    return render_template('6-number_odd_or_even.html', number=n, prop=p)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
